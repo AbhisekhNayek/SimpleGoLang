c2cdc549ef6aadac71c352352b62d9cc8fc9df33
Abhisekh Nayek <115914826+AbhisekhNayek@users.noreply.github.com>
2023-12-10 16:25:26 +0530

    Create TCP_Client.go

---
+++ TCP_Client.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// Connect to the server
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Send a message to the server
+	message := "Hello from client!"
+	conn.Write([]byte(message))
+
+	// Read the response from the server
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	// Print the response
+	fmt.Printf("Response from server: %s\n", buffer[:n])
+}
